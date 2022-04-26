56c3947f97ff4abb50dec7ea748c43f33a5ab6ff
LightSage <lightsage01@gmail.com>
2022-04-26 15:12:32 -0500

    typing

--- api/models.py
+++ api/models.py
@@ -1,29 +1,35 @@
 import random
 from datetime import datetime
-from typing import List, Optional
+from typing import TYPE_CHECKING, Any, Dict, List, Optional
+
+if TYPE_CHECKING:
+    from typing_extensions import Self
+
+
+App = Dict[str, Any]
 
 
 class Universal_DB:
     __slots__ = ("cache", "integrity")
 
-    def __init__(self, cache: dict, integrity: datetime) -> None:
-        self.cache: dict = cache
+    def __init__(self, cache: List[App], integrity: datetime) -> None:
+        self.cache: List[App] = cache
         self.integrity: datetime = integrity
 
     def get_app_names(self) -> List[str]:
         return [app['title'] for app in self.cache]
 
-    def get_app(self, application: str) -> Optional[dict]:
+    def get_app(self, application: str) -> Optional[App]:
         for app in self.cache:
             if app['title'] == application:
                 return app
         return None
 
-    def get_random_app(self) -> dict:
+    def get_random_app(self) -> App:
         return random.choice(self.cache)
 
     @classmethod
-    async def from_redis(cls, pool) -> str:
+    async def from_redis(cls, pool) -> Self:
         integrity = await pool.get("udb:integrity")
         cache = await pool.get("udb:cache")
         return cls(cache, integrity)
