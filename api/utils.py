f0a311fd2ac682a56aa46b3164dc44a7f36344a0
LightSage <lightsage01@gmail.com>
2022-08-05 08:33:47 -0500

    Add log_exception util

--- api/utils.py
+++ api/utils.py
@@ -1,6 +1,21 @@
+from __future__ import annotations
+
+import asyncio
+
 import aioredis
+import sentry_sdk
 
 import config
+
+
+def log_exception(task: asyncio.Task):
+    try:
+        exc = task.exception()
+    except asyncio.CancelledError:
+        return
+
+    sentry_sdk.capture_exception(exc)
+    print(f"An exception occurred {exc}")
 
 
 async def setup_redis() -> aioredis.Redis:
