54425853e55b9238610a2474ae434ccded5b7747
LightSage <lightsage01@gmail.com>
2022-09-28 09:07:00 -0500

    wip

--- api/models.py
+++ api/models.py
@@ -3,7 +3,7 @@ from __future__ import annotations
 import json
 import random
 from datetime import datetime
-from typing import TYPE_CHECKING, Any, Dict, List, Optional
+from typing import TYPE_CHECKING, Any, Dict, List, Literal, Optional, TypedDict
 
 import aioredis
 
@@ -39,3 +39,57 @@ class Universal_DB:
         cache = await pool.get("udb:cache")
         cache = json.loads(cache)
         return cls(cache, datetime.fromisoformat(integrity))
+
+
+class Application(TypedDict):
+    # This is kinda a guess, hopefully it's right.
+    updated: Optional[str]
+    categories: Optional[List[str]]
+    image: Optional[str]
+    # keys I can (hopefully) be certain on existing and being the correct type
+    title: str
+    author: str
+    avatar: str
+    version: str
+    systems: List[Literal['3DS', 'DS']]
+    downloads: Dict[str, ApplicationDownload]
+    created: str
+    screenshots: Optional[List[ApplicationScreenshot]]
+    scripts: Optional[Dict[str, ApplicationScript]]
+    priority: Optional[bool]
+    license: Optional[str]
+    # Guessing at this point, kms
+    # Icons????
+    icon: str
+    icon_index: int
+    # IDK
+    description: Optional[str]
+    # colors
+    color: str
+    color_bg: str
+    # update notes
+    update_notes: str
+    update_notes_md: str
+    # urls
+    urls: List[str]
+
+
+
+class ApplicationDownload(TypedDict):
+    size: int
+    size_str: str
+    url: str
+
+
+class ApplicationScreenshot(TypedDict):
+    description: str
+    url: str
+
+
+class ApplicationScript(TypedDict):
+    type: str
+    directory: Optional[str]
+    file: Optional[str]
+    input: Optional[str]
+    output: Optional[str]
+
